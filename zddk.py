b3bd6a3c44bace03189f44f00908042ea07cfaf8
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2022-09-30 13:44:11 +0800

    Update zddk.py

--- zddk.py
+++ zddk.py
@@ -18,8 +18,13 @@ import os
 login_header={
         'Host': 'ykm.cqsdzy.com',
         'Connection': 'Keep-Alive',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2102J2SC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.96 Mobile Safari/537.36'
-
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2102J2SC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.96 Mobile Safari/537.36',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ru;q=0.6,zh-TW;q=0.5'
+   
 }
 login_url='http://ykm.cqsdzy.com/h5/clockin/index?type=qw&uuid=bf26aefa7f6c4c1098a0a72933253c85'
 request=requests.get(url=login_url,headers=login_header)
