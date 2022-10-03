885ebae049dea5bc287ecdf37cbe4b5682a5bf57
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2022-10-03 11:56:46 +0800

    Update zddk.py

--- zddk.py
+++ zddk.py
@@ -25,7 +25,7 @@ login_url='http://ykm.cqsdzy.com/h5/clockin/index?type=qw&uuid='+uuid
 request=requests.get(url=login_url,headers=login_header)
 #login_data=request.text#登陆成功后返回的信息
 time.sleep(1)
-print(login_data)
+#print(login_data)
 
 login_data2=json.loads(request.text)
 time.sleep(1)
